05f38415cbdff99b79a5ed1ea54ff6ca22b949f0
Nana Kwesi Gyeni <ngyeni@byupathway.edu>
2026-02-04 22:00:37 +0000

    update

--- nextjs-dashboard/app/customers/page.tsx
+++ nextjs-dashboard/app/customers/page.tsx
@@ -5,5 +5,31 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <p>Customers Page</p>;
+  const customers = [
+    { id: 1, name: 'Isaac Newton', email: 'ikenewton@gmail.com' },
+    { id: 2, name: 'Joshua King', email: 'joking@gmail.com' },
+    { id: 3, name: 'Courage Badioo', email: 'hashtag@gmail.com' },
+    { id: 4, name: 'Benjiman Sesko', email: 'sesko30@gmail.com' },
+    { id: 5, name: 'Hugo Frimpong', email: 'ekitike22@gmail.com' },
+  ];
+
+  return (
+    <div className="w-full">
+      <h1 className="mb-4 text-2xl font-semibold">Customers</h1>
+      <div className="mt-6 flow-root">
+        <div className="overflow-x-auto">
+          <div className="inline-block min-w-full align-middle">
+            <div className="overflow-hidden rounded-lg bg-gray-50 p-2">
+              {customers.map((customer) => (
+                <div key={customer.id} className="mb-2 bg-white p-4 rounded">
+                  <p className="font-medium">{customer.name}</p>
+                  <p className="text-sm text-gray-600">{customer.email}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
